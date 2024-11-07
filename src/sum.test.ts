367f5fe7a3a8bcbd7b579b03b87095b0236febe4
krysthecoder <krysthopher@gmail.com>
2024-11-06 22:05:47 -0600

    refactor(package.json): added jest test script

---
+++ src/sum.test.ts
@@ -0,0 +1,5 @@
+import { sum } from "./sum";
+
+it('this is the sum test', ()=>{
+  expect(sum(3,2)).toBe(5)
+})
---
+++ src/sum.ts
@@ -0,0 +1,3 @@
+export const sum = (a: number, b: number): number => {
+  return a + b;  // return the sum of a and b as a number
+}
