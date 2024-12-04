4bee70a6ee73faf9bdcb1520616536f2161b25b1
AlvisonHunterArnuero <alvison@gmail.com>
2024-12-03 19:29:58 -0600

    feat(comps): added Login demo, along with story, logintexfields data, types and textfield group

---
+++ src/components/Login/LoginTextFieldsData.ts
@@ -0,0 +1,14 @@
+export const LoginTextFields = [
+  {
+    id: 'outline-email',
+    label: 'Email Address',
+    variant: 'outlined',
+    type: 'email',
+  },
+  {
+    id: 'outline-pwd',
+    label: 'Password',
+    variant: 'outlined',
+    type: 'password',
+  },
+];
