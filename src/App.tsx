74a2096ab21c573514843ae2bfe58e16693d43fe
Carlos M <53409854+Cema2019@users.noreply.github.com>
2024-12-10 16:26:52 -0600

    feat(app): update App.tsx to include login modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,8 @@
-import { Box, Typography } from '@mui/material';
-import Button from '@mui/material/Button';
-import { useState } from 'react';
+
 
 export default function App() {
-  const [counter, setCounter] = useState(0);
+
   return (
-    <Box
-      display="flex"
-      alignItems="center"
-      justifyContent="center"
-      flexDirection="column"
-      sx={{
-        height: '100vh',
-        backgroundColor: '#121212'
-      }}
-    >
-      <Typography variant="h4" sx={{ color: '#fff', marginBottom: 2 }}>
-        Counter: {counter}
-      </Typography>
-      <Button
-        onClick={() => setCounter(counter + 1)}
-        variant="contained"
-        sx={{
-          backgroundColor: 'green'
-        }}
-      >
-        +1
-      </Button>
-    </Box>
+    
   );
 }
